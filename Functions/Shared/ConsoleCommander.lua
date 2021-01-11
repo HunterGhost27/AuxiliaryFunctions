692b9e5b79f290b51c19673c41d9edcedf50b12d
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-01-11 12:02:20 +0530

    Update ConsoleCommander.lua

--- Functions/Shared/ConsoleCommander.lua
+++ Functions/Shared/ConsoleCommander.lua
@@ -5,19 +5,19 @@
 --  COMMAND OBJECT
 --  ==============
 
----@class Command
+---@class Command @Console-Command Template
 ---@field Name string Command Name
 ---@field Action function Command Action
 ---@field Context string Context: "Shared", "Server" or "Client"
 ---@field Description string Help-Messsage
----@field Params table Parameters
+---@field Params table<number, string> Parameters
 ---@field New function Creates new instance
 Command = {
     ['Name'] = "",
     ['Action'] = function() end,
     ['Context'] = "Shared",
     ['Description'] = "",
-    ['Params'] = {}
+    -- ['Params'] = {}
 }
 
 ---Instantiate new Command Object
@@ -49,25 +49,32 @@ function ConsoleCommander:Register(CMD)
     self[CMD.Name] = Command:New(CMD)
 end
 
+--  HELP MESSAGE
+--  ============
+
 ---Help-Message Console-Command
 ---@param target string Command-Name or ""
 function ConsoleCommanderHelp(target)
     local target = target or ""
     local helpMsg = "\n"
+    local repNo = 100
+
+    helpMsg = helpMsg .. string.rep("=", repNo) .. "\n"
     if ConsoleCommander[target] and isValidContext(ConsoleCommander[target]) then
-        helpMsg = target .. " - " .. ConsoleCommander[target].Description
+        helpMsg = target .. ": " .. ConsoleCommander[target].Description
+        if ConsoleCommander[target].Params then helpMsg = helpMsg .. string.rep("-", repNo) .. "\n" end
         for key, value in ipairs(ConsoleCommander[target].Params) do helpMsg = "\n" .. helpMsg .. "\t" .. "Parameter" .. key .. ": " .. value end
+
     else
-        helpMsg = helpMsg .. string.rep("=", 70) .. "\n"
         for name, CMD in pairs(ConsoleCommander) do
             if type(CMD) == 'table' and isValidContext(CMD) then
                 helpMsg = helpMsg .. "COMMAND: ".. name .. "\nDESCRIPTION: " .. CMD.Description .. "\n"
-                helpMsg = helpMsg .. string.rep("-", 70) .. "\n"
+                helpMsg = helpMsg .. string.rep("-", repNo) .. "\n"
             end
         end
         helpMsg = helpMsg .. "!S7_Forgetinator Help <CommandName> for more info\n"
-        helpMsg = helpMsg .. string.rep("=", 70) .. "\n"
     end
+    helpMsg = helpMsg .. string.rep("=", repNo) .. "\n"
     Debug:FWarn(helpMsg)
 end
 
@@ -78,6 +85,9 @@ ConsoleCommander:Register({
     ['Description'] = "Displays a list of all console-commands",
     ['Params'] = {[1] = "Target Command-Name"}
 })
+
+--  REGISTER CONSOLE-COMMANDER
+--  ==========================
 
 Ext.RegisterConsoleCommand(IDENTIFIER, function (cmd, command, ...)
     if not ValidString(command) then Debug:FError('Invalid Command. Try !' .. IDENTIFIER .. ' Help'); return end
