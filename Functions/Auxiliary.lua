72bceacf86a86525e9b687d59a4bf8aeb0674b6c
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-01-12 17:55:47 +0530

    Update Updater

--- Functions/Auxiliary.lua
+++ Functions/Auxiliary.lua
@@ -1,8 +1,10 @@
---  ---------------------
---  =====================
-Ext.Require('Config.lua')
---  =====================
---  ---------------------
+------------------------------------
+--==================================
+MODINFO = Ext.GetModInfo('>>UUID<<')
+IDENTIFIER = '>>IDENTIFIER<<'
+PersistentVars = {}
+--==================================
+------------------------------------
 
 --  ===================
 --  AUXILIARY FUNCTIONS
@@ -12,9 +14,12 @@ Ext.Require('Functions/Shared/General.lua')
 Ext.Require('Functions/Shared/Strings.lua')
 Ext.Require('Functions/Shared/Tables.lua')
 Ext.Require('Functions/Shared/Files.lua')
+Ext.Require('Functions/Shared/Debug.lua')
+
 Ext.Require('Functions/Shared/SettingsManager.lua')
 Ext.Require('Functions/Shared/Versioning.lua')
 Ext.Require('Functions/Shared/Centralize.lua')
 Ext.Require('Functions/Shared/Networker.lua')
-Ext.Require('Functions/Shared/Debug.lua')
 Ext.Require('Functions/Shared/ConsoleCommander.lua')
+
+Ext.Require('Functions/Shared/Updater.lua')
