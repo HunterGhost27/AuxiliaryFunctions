7254c287583d3a08386678dfe06cd88291ef3412
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-01-15 22:41:49 +0530

    Update Strings.lua

--- Functions/Shared/Strings.lua
+++ Functions/Shared/Strings.lua
@@ -71,12 +71,14 @@ function Stringer:LineBreak(char) self:Add(string.rep(char, self.MaxLen)) end
 function Stringer:Tabulate(t)
     if type(t) ~= 'table' then return end
     local t = Rematerialize(t) or {}
-    local keys = ExtractKeys(t)
+
     local maxLen = 0
+    local keys = ExtractKeys(t)
     for _, key in pairs(keys) do
         local len = string.len(tostring(key))
         if len > maxLen then maxLen = len end
     end
+
     local displayStr = "\n"
     for key, value in pairs(t) do
         key = tostring(key)
