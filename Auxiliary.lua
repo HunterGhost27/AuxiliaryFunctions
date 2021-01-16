518364f2b95c8d8ce9d512ccd00319850ba2f0fb
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-01-16 22:03:33 +0530

    Update AuxFunctions

--- Auxiliary.lua
+++ Auxiliary.lua
@@ -6,6 +6,7 @@ IDENTIFIER = '>>IDENTIFIER<<'
 ---@class MODINFO: ModInfo
 ---@field ModVersion string
 ---@field ModSettings table
+---@field DefaultSettings table
 ---@field SubdirPrefix string
 MODINFO = Ext.GetModInfo('>>UUID<<')
 MODINFO.DefaultSettings = {}
