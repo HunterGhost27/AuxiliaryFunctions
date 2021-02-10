3bf721d797633913920b5f84a67c604622a709e6
Shresht Srivastav <Shresht7@users.noreply.github.com>
2021-02-10 16:37:52 +0530

    Init commander

--- bin/auxFunctions.js
+++ bin/auxFunctions.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
+//=================
+
+//  =======
+//  IMPORTS
+//  =======
 
 require = require("esm")(module /*, options */);
-require("../src/cli.js").cli(process.argv);
+const app = require("../package.json");
+const program = require("commander");
+
+//  ============
+//  MAIN COMMAND
+//  ============
+
+program
+  .name("AuxFunctions")
+  .version(app.version)
+  .action(() => console.log("Sync"))
+  .parse(process.argv);
